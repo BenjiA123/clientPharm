dde92d5c659c2040fa7c72d38cad3316229f5df5
Akande Benji <adeoul001@gmail.com>
2021-09-26 14:33:11 -0700

    Configured dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { AuthGuard } from '../auth/auth.gaurd';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
@@ -11,18 +10,17 @@ import { AuthService } from '../auth/auth.service';
 export class DashboardComponent implements OnInit, OnDestroy {
 
 
-  public permitted = false
 
   public dashboardData: any[] = [
 
+    {
+      title: 'Transactions', subtitle: 'Contact Users', icon: 'MessageIcon', description: 'Monitor the statuses of all your transactions', link: "transactions"
+    },
     {
       title: 'Message', subtitle: 'Interact directly with users', icon: 'chat', description: 'Send them direct messages with emails', link: "message"
     },
     {
       title: 'Sources', subtitle: 'Get all vendors', icon: 'home', description: 'Get all your sources at a go', link: "sources"
-    },
-    {
-      title: 'Transactions', subtitle: 'Contact Users', icon: 'MessageIcon', description: 'Monitor the statuses of all your transactions', link: "transactions"
     },
     {
       title: 'Charts', subtitle: 'View your graphs', icon: 'chart', description: 'View how transactions change with the grephs', link: "charts"
@@ -34,30 +32,27 @@ export class DashboardComponent implements OnInit, OnDestroy {
       title: 'Create', subtitle: 'Create Sources and drugs', icon: 'add', description: 'Create drugs and sources with ease', link: "create"
     },
     {
-      title: 'Sign Up', subtitle: 'Create Employees', icon: 'MessageIcon', description: 'Create Your team of employees', link: "/auth/create-user"
+      title: 'Sign Up', subtitle: 'Create Employees', icon: 'MessageIcon', description: 'Create Your team of employees', link: "auth/create-user"
     },
     {
-      title: 'Drugs', subtitle: 'Create Transactions', icon: 'MessageIcon', description: 'Creates Users Transactions from Drugs', link: "/drugs"
+      title: 'Drugs', subtitle: 'Create Transactions', icon: 'MessageIcon', description: 'Creates Users Transactions from Drugs', link: "drugs"
     },
   ]
 
   private roleListenerSubs: Subscription
-  private userRole = '';
+  private userRole: string;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService) { }
 
+  ngOnInit(): void {
     this.roleListenerSubs = this.authService.getRoleStatusListener().subscribe(
       (userRole) => {
         this.userRole = userRole
-        console.log(userRole == 'MD')
       }
     )
 
-
   }
 
-  ngOnInit(): void {
-  }
 
 
   ngOnDestroy(): void {
